a7c0a3ce4b4678729545c7cb45189553e71ecd25
Luis Geniole <luis.geniole@gmail.com>
2019-11-12 22:46:50 -0300

    add useCallback

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useMemo, useCallback } from 'react';
 
 function App() {
   const [techs, setTechs] = useState([]);
   const [newTech, setNewTech] = useState('');
 
-  const handleAdd = () => {
+  // Reloads function only when techs&newTech change
+  const handleAdd = useCallback(() => {
     setTechs([...techs, newTech]);
     setNewTech('');
-  };
+  }, [techs, newTech]);
 
   // Executes once, when page is loaded
   useEffect(() => {
@@ -26,8 +27,12 @@ function App() {
     localStorage.setItem('techs', JSON.stringify(techs));
   }, [techs]);
 
+  const techCount = useMemo(() => techs.length, [techs]);
+
   return (
     <>
+      <strong>You have {techCount} techs</strong>
+      <br />
       <ul>
         {techs.map(t => (
           <li key={t}>{t}</li>
