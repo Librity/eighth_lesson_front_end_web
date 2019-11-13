2d636c2d594c404fe7406babd6796e8d5872a9ac
Luis Geniole <luis.geniole@gmail.com>
2019-11-12 22:21:12 -0300

    add setState

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
+  const [techs, setTechs] = useState(['NodeJS', 'Rails', 'Django', 'Laravel']);
+  const [newTech, setNewTech] = useState('');
+
+  const handleAdd = () => {
+    setTechs([...techs, newTech]);
+    setNewTech('');
+  };
+
   return (
-    <h1>Sup homes,</h1>
+    <>
+      <ul>
+        {techs.map(t => (
+          <li key={t}>{t}</li>
+        ))}
+      </ul>
+      <input value={newTech} onChange={e => setNewTech(e.target.value)} />
+      <button type="button" onClick={handleAdd}>
+        Add
+      </button>
+    </>
   );
 }
 
